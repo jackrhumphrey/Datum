380753185b3f4dc5ed430610c9a1e5379f4fd2c3
jackrhumphrey <jack.humphrey@xtra.co.nz>
2020-01-05 14:42:12 +1300

    header

--- src/App.tsx
+++ src/App.tsx
@@ -11,118 +11,147 @@ import TechnologyItem from "./components/TechnologyItem";
 import TeamItem from "./components/TeamItem";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 
-const App: React.FC = () => {
-  const theme = createMuiTheme({
-    palette: {
-      primary: {
-        // light: will be calculated from palette.primary.main,
-        main: "#1f6b8b"
-        // dark: will be calculated from palette.primary.main,
-        // contrastText: will be calculated to contrast with palette.primary.main
-      }
-    },
-  });
 
-  return (
-    <div>
-      <ThemeProvider theme={theme}>
-        <Header />
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: "#1f6b8b"
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    }
+  }
+});
 
-        <Box className="technology">
-          <Container>
-            <Slide title="header" subtitle="Subheader" />
-            <div className="container">
-              <Grid container spacing={0} className="container">
-                <Grid item xs={3}>
-                  <TechnologyItem
-                    icon="Empty"
-                    title="Title"
-                    content="Content"
-                  />
-                </Grid>
-                <Grid item xs={3}>
-                  <TechnologyItem
-                    icon="Empty"
-                    title="Title"
-                    content="Content"
-                  />
-                </Grid>
-                <Grid item xs={3}>
-                  <TechnologyItem
-                    icon="Empty"
-                    title="Title"
-                    content="Content"
-                  />
-                </Grid>
-                <Grid item xs={3}>
-                  <TechnologyItem
-                    icon="Empty"
-                    title="Title"
-                    content="Content"
-                  />
-                </Grid>
-              </Grid>
-            </div>
-          </Container>
-        </Box>
+interface IProps {}
 
-        <Box className="team">
-          <Container>
-            <Slide title="header" subtitle="Subheader" />
-            <div className="container">
-              <Grid container spacing={1}>
-                <Grid item xs={3}>
-                  <TeamItem
-                    photo="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-                    name="Name"
-                    role="Role"
-                    description="Description"
-                  />
-                </Grid>
-                <Grid item xs={3}>
-                  <TeamItem
-                    photo="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-                    name="Name"
-                    role="Role"
-                    description="Description"
-                  />
-                </Grid>
-                <Grid item xs={3}>
-                  <TeamItem
-                    photo="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-                    name="Name"
-                    role="Role"
-                    description="Description"
-                  />
-                </Grid>
-                <Grid item xs={3}>
-                  <TeamItem
-                    photo="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
-                    name="Name"
-                    role="Role"
-                    description="Description"
-                  />
-                </Grid>
-              </Grid>
-            </div>
-          </Container>
-        </Box>
+interface IState {
+  collapsed: boolean;
+}
 
-        <Box className="contact">
-          <Container>
-            <Slide title="header" subtitle="Subheader" />
-            <Contact />
-          </Container>
-        </Box>
+class App extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
 
-        <Footer />
-      </ThemeProvider>
-    </div>
-  );
-};
+    this.state = {
+      collapsed: true
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    if (this.state.collapsed == true) {
+      this.setState({
+        collapsed: false
+      });
+    } else {
+      this.setState({
+        collapsed: true
+      });
+    }
+    $('.right').toggleClass('rightactive');
+    $('.logo').toggleClass('logoactive');
+  }
+
+  render() {
+    return (
+      <div>
+        <ThemeProvider theme={theme}>
+          <Header collapsed={this.state.collapsed} toggle={this.toggle} />
+
+          <Box className="technology">
+            <Container>
+              <Slide title="header" subtitle="Subheader" />
+              <div className="container">
+                <Grid container spacing={0} className="container">
+                  <Grid item xs={3}>
+                    <TechnologyItem
+                      icon="Empty"
+                      title="Title"
+                      content="Content"
+                    />
+                  </Grid>
+                  <Grid item xs={3}>
+                    <TechnologyItem
+                      icon="Empty"
+                      title="Title"
+                      content="Content"
+                    />
+                  </Grid>
+                  <Grid item xs={3}>
+                    <TechnologyItem
+                      icon="Empty"
+                      title="Title"
+                      content="Content"
+                    />
+                  </Grid>
+                  <Grid item xs={3}>
+                    <TechnologyItem
+                      icon="Empty"
+                      title="Title"
+                      content="Content"
+                    />
+                  </Grid>
+                </Grid>
+              </div>
+            </Container>
+          </Box>
+
+          <Box className="team">
+            <Container>
+              <Slide title="header" subtitle="Subheader" />
+              <div className="container">
+                <Grid container spacing={1}>
+                  <Grid item xs={3}>
+                    <TeamItem
+                      photo="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+                      name="Name"
+                      role="Role"
+                      description="Description"
+                    />
+                  </Grid>
+                  <Grid item xs={3}>
+                    <TeamItem
+                      photo="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+                      name="Name"
+                      role="Role"
+                      description="Description"
+                    />
+                  </Grid>
+                  <Grid item xs={3}>
+                    <TeamItem
+                      photo="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+                      name="Name"
+                      role="Role"
+                      description="Description"
+                    />
+                  </Grid>
+                  <Grid item xs={3}>
+                    <TeamItem
+                      photo="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+                      name="Name"
+                      role="Role"
+                      description="Description"
+                    />
+                  </Grid>
+                </Grid>
+              </div>
+            </Container>
+          </Box>
+
+          <Box className="contact">
+            <Container>
+              <Slide title="header" subtitle="Subheader" />
+              <Contact />
+            </Container>
+          </Box>
+
+          <Footer />
+        </ThemeProvider>
+      </div>
+    );
+  }
+}
 
 export default App;
-
-{
-  /* <Button variant="contained" color="primary"></Button> */
-}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,35 @@
 import * as React from "react"; // to make JSX compile
 
-const Header: React.FunctionComponent = () => {
+type MenuProps = {
+  collapsed: boolean;
+  toggle: () => void;
+};
+
+const Header: React.FunctionComponent<MenuProps> = props => {
   return (
     <div className="header">
       <div className="logo">Logo</div>
-      <div className="links">
-        <a href="#">Link</a>
-        <a href="#">Link</a>
-        <a href="#">Link</a>
-        <a href="#">Link</a>
-        <a href="#">Link</a>
+      <div className="right">
+        {props.collapsed ? (
+          <div className="links">
+            <div className="min" onClick={props.toggle}>
+              +
+            </div>
+          </div>
+        ) : (
+          <div className="links">
+            <div className="max" onClick={props.toggle}>
+              -
+            </div>
+            <div>
+              <a href="#">Link</a>
+              <a href="#">Link</a>
+              <a href="#">Link</a>
+              <a href="#">Link</a>
+              <a href="#">Link</a>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
